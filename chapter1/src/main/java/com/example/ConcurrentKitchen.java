eed4e9d22cd461c3bf3f340274abba35f81fe98a
michail.almyros <m.almyros@gmail.com>
2025-05-10 21:37:20 +0100

    - Adding initial examples from chapter 1

--- chapter1/src/main/java/com/example/ConcurrentKitchen.java
+++ chapter1/src/main/java/com/example/ConcurrentKitchen.java
@@ -1,4 +1,41 @@
 package com.example;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ConcurrentKitchen {
+
+    public static void main(String[] args) {
+
+        try (ExecutorService executorService = Executors.newFixedThreadPool(2)) {
+
+            Future<?> choppingTask = executorService.submit(() -> {
+                System.out.println("Chopping food...");
+                try {
+                    Thread.sleep(600);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+
+
+            Future<?> grillingTask = executorService.submit(() -> {
+                System.out.println("Grilling food...");
+                try {
+                    Thread.sleep(600);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            });
+
+            try {
+                choppingTask.get();
+                grillingTask.get();
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- chapter1/src/main/java/com/example/ParallelKitchen.java
+++ chapter1/src/main/java/com/example/ParallelKitchen.java
@@ -1,4 +1,18 @@
 package com.example;
 
+import java.util.stream.IntStream;
+
 public class ParallelKitchen {
+
+    public static void main(String[] args) {
+
+        IntStream.range(0, 10).parallel().forEach(i -> {
+            System.out.println("Cooking dish #" + i + " in parallel...");
+            try {
+                Thread.sleep(600);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        });
+    }
 }
